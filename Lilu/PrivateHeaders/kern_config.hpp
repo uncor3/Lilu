d7b0eadc0cede0c8fce324a288e5222c9bf1ef60
uncor3 <contactuncor3@gmail.com>
2025-06-10 19:10:54 +0000

    add initial Tahoe support

--- Lilu/PrivateHeaders/kern_config.hpp
+++ Lilu/PrivateHeaders/kern_config.hpp
@@ -58,7 +58,7 @@ private:
 	/**
 	 * Maxmimum supported kernel version
 	 */
-	static constexpr KernelVersion maxKernel {KernelVersion::Sequoia};
+	static constexpr KernelVersion maxKernel {KernelVersion::Tahoe};
 
 	/**
 	 *  Set once the arguments are parsed
